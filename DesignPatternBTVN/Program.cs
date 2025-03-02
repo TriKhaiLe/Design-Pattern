350bf3feaf198e8486bf36ad41074d17b00ee48c
LeKhaiTri <lekhaitri.2003@gmail.com>
2025-03-02 12:15:15 +0700

    Add project files.

---
+++ DesignPatternBTVN/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatternBTVN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

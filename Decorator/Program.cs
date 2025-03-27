8a06290f4fcd0cf790cea66aeb6ab89dab3d55de
LeKhaiTri <lekhaitri.2003@gmail.com>
2025-03-27 09:50:17 +0700

    decorator v2

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -3,11 +3,21 @@
     using System;
     using System.Collections.Generic;
 
-    abstract class Linh
+    public interface ILinh
     {
-        protected List<string> quanTrangList = new List<string>();
-        protected List<string> vuKhiList = new List<string>();
-        protected List<string> kyNangList = new List<string>();
+        List<string> GetQuanTrangList();
+        List<string> GetVuKhiList();
+        List<string> GetKyNangList();
+        string GetQuanHam();
+        string SetQuanHam(string quanHam);
+        void GetItem();
+    }
+
+    public class LinhCoBan : ILinh
+    {
+        protected List<string> quanTrangList = new List<string> { "Balo", "Quan ao", "Non" };
+        protected List<string> vuKhiList = [];
+        protected List<string> kyNangList = [];
         protected string quanHam = "Chua co";
 
         public List<string> GetQuanTrangList() => quanTrangList;
@@ -15,24 +25,13 @@
         public List<string> GetKyNangList() => kyNangList;
         public string GetQuanHam() => quanHam;
 
-        public void SetQuanHam(string quanHam)
+        public string SetQuanHam(string quanHam)
         {
             this.quanHam = quanHam;
+            return this.quanHam;
         }
 
-        public abstract void GetItem(string quanHam);
-    }
-
-    class LinhCoBan : Linh
-    {
-        public LinhCoBan()
-        {
-            quanTrangList.Add("Balo");
-            quanTrangList.Add("Quan ao");
-            quanTrangList.Add("Non");
-        }
-
-        public override void GetItem(string quanHam)
+        public void GetItem()
         {
             Console.WriteLine("Quan trang: " + string.Join(", ", quanTrangList));
             Console.WriteLine("Vu khi: " + (vuKhiList.Count > 0 ? string.Join(", ", vuKhiList) : "Khong co"));
@@ -42,76 +41,73 @@
         }
     }
 
-    abstract class LinhDecorator : Linh
+    public abstract class LinhDecorator : ILinh
     {
-        protected Linh linh;
+        protected ILinh linh;
 
-        public LinhDecorator(Linh linh)
-        {
-            this.linh = linh;
-            this.quanTrangList = linh.GetQuanTrangList();
-            this.vuKhiList = linh.GetVuKhiList();
-            this.kyNangList = linh.GetKyNangList();
-            this.quanHam = linh.GetQuanHam();
-        }
+        public LinhDecorator(ILinh linh) => this.linh = linh;
 
-        public override void GetItem(string quanHam)
-        {
-            linh.GetItem(quanHam);
-        }
+        public virtual List<string> GetQuanTrangList() => linh.GetQuanTrangList();
+        public virtual List<string> GetVuKhiList() => linh.GetVuKhiList();
+        public virtual List<string> GetKyNangList() => linh.GetKyNangList();
+        public virtual string GetQuanHam() => linh.GetQuanHam();
+        public virtual string SetQuanHam(string quanHam) => linh.SetQuanHam(quanHam);
+        public virtual void GetItem() => linh.GetItem();
     }
 
-    class VuKhi : LinhDecorator
+    public class VuKhi : LinhDecorator
     {
-        public VuKhi(Linh linh, string vuKhi) : base(linh)
-        {
-            vuKhiList.Add(vuKhi);
-        }
+        public VuKhi(ILinh linh, string vuKhi) : base(linh) => linh.GetVuKhiList().Add(vuKhi);
+
+        public override void GetItem() => base.GetItem();
     }
 
-    class KyNang : LinhDecorator
+    public class KyNang : LinhDecorator
     {
-        public KyNang(Linh linh, string kyNang) : base(linh)
-        {
-            kyNangList.Add(kyNang);
-        }
+        public KyNang(ILinh linh, string kyNang) : base(linh) => linh.GetKyNangList().Add(kyNang);
+
+        public override void GetItem() => base.GetItem();
     }
 
-    class QuanHam : LinhDecorator
+    public class QuanHam : LinhDecorator
     {
-        public QuanHam(Linh linh, string quanHamMoi) : base(linh)
+        public QuanHam(ILinh linh, string quanHam) : base(linh)
         {
-            this.quanHam = quanHamMoi;
+            this.linh.SetQuanHam(quanHam);
         }
+
+        public override string GetQuanHam() => base.GetQuanHam();
+
+        public override void GetItem() => base.GetItem();
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Linh linh = new LinhCoBan();
+            ILinh linh = new LinhCoBan();
             Console.WriteLine("Linh ban dau:");
-            linh.GetItem(linh.GetQuanHam());
+            linh.GetItem();
 
             linh = new VuKhi(linh, "Sung");
             Console.WriteLine("Sau khi trang bi sung:");
-            linh.GetItem(linh.GetQuanHam());
+            linh.GetItem();
 
             linh = new KyNang(linh, "Ban sung");
             Console.WriteLine("Sau khi hoc ky nang ban sung:");
-            linh.GetItem(linh.GetQuanHam());
+            linh.GetItem();
 
             linh = new QuanHam(linh, "Thieu uy");
             Console.WriteLine("Sau khi thang cap Thieu uy:");
-            linh.GetItem(linh.GetQuanHam());
+            linh.GetItem();
 
             linh = new QuanHam(linh, "Trung uy");
             Console.WriteLine("Sau khi thang cap Trung uy:");
-            linh.GetItem(linh.GetQuanHam());
+            linh.GetItem();
 
             linh = new QuanHam(linh, "Quan nhan");
             Console.WriteLine("Sau khi giang cap xuong Quan nhan:");
-            linh.GetItem(linh.GetQuanHam());
+            linh.GetItem();
         }
     }
 }
